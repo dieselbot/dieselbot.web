2aee2e75897110065097a1858a6127489a9f7a56
EFS Helper <askmrtillman@gmail.com>
2024-12-30 19:49:12 -0500

    send search results to redis; fix display name in search results view

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,8 @@
 const FuelSolution = require('../efshelper.core/domain/fuel.solution');
 const SearchService = require('../efshelper.core/services/search');
 const SearchUseCase = require('../efshelper.core/application/search.usecase');
+const { FUEL_STOP_FOUND_CHANNEL } = require('../efshelper.core/common/constants.json')
+const { createClient } = require('../efshelper.core/node_modules/redis');
 
 async function searchRoute(request, reply) {
     const search_usecase = new SearchUseCase();
@@ -9,6 +11,15 @@ async function searchRoute(request, reply) {
     const params = {
         results: await search_usecase.execute()
     }
+
+    const client = createClient();
+        
+    await client.connect();
+    
+    await client.publish(FUEL_STOP_FOUND_CHANNEL, JSON.stringify(params.results));
+    
+    await client.disconnect()
+
     return reply.view("/src/pages/search_results.hbs", params);
 }
 
