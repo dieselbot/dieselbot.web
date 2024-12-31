4293c4b128477a10aa4b08fdd1a02156ad2e2e3e
EFS Helper <askmrtillman@gmail.com>
2024-12-31 11:38:09 -0500

    use updated constant for fuelstop.found; minor refactor in search service

--- routes/search.js
+++ routes/search.js
@@ -1,19 +1,18 @@
 const FuelSolution = require('../efshelper.core/domain/fuel.solution');
 const SearchService = require('../efshelper.core/services/search');
 const SearchUseCase = require('../efshelper.core/application/search.usecase');
-const { FUEL_STOP_FOUND_CHANNEL } = require('../efshelper.core/common/constants.json')
+const { FUELSTOP } = require('../efshelper.core/common/constants.json')
 
 async function searchRoute(request, reply) {
     const search_usecase = new SearchUseCase();
     search_usecase.fuel_solution = new FuelSolution(request.body.fuel_solution);
     search_usecase.search_service = new SearchService();
-    const params = {
-        results: await search_usecase.execute()
-    }
     
-    await this.redisClient.publish(FUEL_STOP_FOUND_CHANNEL, JSON.stringify(params.results));
+    const results = await search_usecase.execute();
+    
+    await this.redisClient.publish(FUELSTOP.FOUND, JSON.stringify(results));
 
-    return reply.view("/src/pages/search_results.hbs", params);
+    return reply.view("/src/pages/search_results.hbs", { results });
 }
 
 module.exports = searchRoute
