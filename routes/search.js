8bf75a4a65fa7427a83b73a24b0ea280d44865ac
EFS Helper <askmrtillman@gmail.com>
2025-01-02 22:07:45 -0500

    use constructor injection; format document

--- routes/search.js
+++ routes/search.js
@@ -3,12 +3,12 @@ const SearchUseCase = require('../efshelper.core/application/search.usecase');
 const FuelStopService = require('../efshelper.core/services/fuelstop');
 
 async function searchRoute(request, reply) {
-    const search_usecase = new SearchUseCase();
-    search_usecase.fuel_solution = new FuelSolution(request.body.fuel_solution);
+    const search_usecase = new SearchUseCase(
+        new FuelSolution(request.body.fuel_solution));
 
     const results = await search_usecase.execute();
 
-    if(search_usecase.missing_fuel_stops.length > 0){
+    if (search_usecase.missing_fuel_stops.length > 0) {
         const service = new FuelStopService();
         service.post(search_usecase.missing_fuel_stops);
     }
